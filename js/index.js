64a7a634f44e93bf7c9e86e9d1007dfdbfbf306b
AnubisN <nirmalthing123@gmail.com>
2021-05-25 08:51:04 +0545

    Slider and service section

--- js/index.js
+++ js/index.js
@@ -60,33 +60,11 @@ $(document).ready(function(){
 });
 
 /*Testimonial slider*/
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev');
-const slides = document.querySelectorAll('.testimonial-section .slide');
-
-let index = 0;
-display(index);
-function display (index) {
-	slides.forEach((slide) => {
-		slide.style.display = 'none';
-	});
-	slides[index].style.display = 'flex';
-}
-
-function nextSlide () {
-	index++;
-	if (index > slides.length - 1) {
-		index = 0;
-	}
-	display(index);
-}
-function prevSlide () {
-	index--;
-	if (index < 0) {
-		index = slides.length - 1;
-	}
-	display(index);
-}
-
-next.addEventListener('click', nextSlide);
-prev.addEventListener('click', prevSlide);
+$('#tes-area').owlCarousel({
+  singleItem:true,
+  transitionStyle:'fadeUp',
+  mouseDrag: false,
+  touchDrag:false,
+  autoPlay: 3000,
+  pagination: true
+})
