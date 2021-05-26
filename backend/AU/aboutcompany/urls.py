e168e444f977a973e0d06efa071736fd26268e8b
{shiva} <girishiva93@gmail.com>
2021-05-26 17:09:52 +0545

    Adding About Company Page

--- backend/AU/aboutcompany/urls.py
+++ backend/AU/aboutcompany/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.about, name='about'),
+    path('aboutteam/', views.meetourteam, name='meetourteam'),
+    path('messagefromCEO/', views.MessageFromCEO, name='MessageFromCEO')
 ]
--- backend/AU/aboutcompany/views.py
+++ backend/AU/aboutcompany/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, 'pages/aboutcompany.html')
+
+
+def meetourteam(request):
+    return render(request, 'pages/meetourteam.html')
+
+
+def MessageFromCEO(request):
+    return render(request, 'pages/messagefromceo.html')
