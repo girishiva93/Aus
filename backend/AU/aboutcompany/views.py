03460297d64b1e5f8959e4c9afb3a31b7acad0d4
{shiva} <girishiva93@gmail.com>
2021-05-27 18:15:11 +0545

    Making About Team Dyanamic

--- backend/AU/aboutcompany/views.py
+++ backend/AU/aboutcompany/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import AboutCompany
+from meetteam.models import AboutTeam
 
 
 def about(request):
-    aboutcompany = AboutCompany.objects.all()
+    aboutcompany = AboutCompany.objects.get(id=1)
+    aboutteam = AboutTeam.objects.all()
     context = {
         'aboutcompany': aboutcompany,
+        'aboutteam': aboutteam,
     }
     return render(request, 'pages/aboutcompany.html', context)
