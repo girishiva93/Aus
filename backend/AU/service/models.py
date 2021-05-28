3b99ea4faaf24cecf5685675119125b46767073b
{shiva} <girishiva93@gmail.com>
2021-05-28 17:19:15 +0545

    About us Page Complete

--- backend/AU/service/models.py
+++ backend/AU/service/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    hero_img = models.ImageField(upload_to='photos/%Y/%m/%d')
+    hero_title = models.CharField(max_length=300)
+    hero_slogan = models.CharField(max_length=300)
+    hero_subject_name = models.CharField(max_length=100)
+    hero_subject_name_I = models.CharField(max_length=100)
+    hero_button_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hero_subject_name
+
+
+class Courses(models.Model):
+    course_img = models.ImageField(upload_to='photos/%Y/%m/%d')
+    course_name = models.CharField(max_length=300)
+    teacher_name = models.CharField(max_length=100)
+    course_start_date = models.DateField()
+    course_description = models.TextField(max_length=200)
+    course_button_name = models.CharField(max_length=100)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.course_name
--- backend/AU/service/views.py
+++ backend/AU/service/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Service, Courses
 
 
 def service(request):
-    return render(request, 'pages/service.html')
+    service = Service.objects.get(pk=1)
+    courses = Courses.objects.all()
+
+    context = {
+        'service': service,
+        'courses': courses,
+    }
+    return render(request, 'pages/service.html', context)
