4c221a7eba238afbef5858b0b7784c2063daf33d
{shiva} <girishiva93@gmail.com>
2021-05-29 18:14:10 +0545

    Adding Blog and Doing SEO on it

--- backend/AU/indexblog/models.py
+++ backend/AU/indexblog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+from django.urls import reverse
+
+
+class Blog(models.Model):
+    """
+    Blog Model
+
+    Fields
+        blog_title
+        blog_tag
+        blog_image
+        blog_describe
+        is_published
+        list_date
+        ....rest
+    """
+    blog_title = models.CharField(max_length=700)
+    blog_tag = models.CharField(max_length=400)
+    blog_image = models.ImageField(
+        upload_to='photos/%Y/%m/%d', blank=False, max_length=500)
+    author = models.CharField(max_length=200)
+    blog_describe = models.TextField(blank=False)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.blog_title
+
+    def get_absolute_url(self):
+        return reverse('blogs', args=[self.id, ])
--- backend/AU/indexblog/views.py
+++ backend/AU/indexblog/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 
 def indexblog(request):
+    """
+            Display Blog  page
 
-    return render(request, 'pages/blog.html')
+            **Context**
+
+            ``indexblog``
+                An instance of :model:`indexblog.Course`
+
+            **Templates**
+            :template:`pages/blog.html`
+        """
+    blogs = Blog.objects.order_by('-list_date').filter(is_published=True)
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'pages/blog.html', context)
+
+
+def blog(request, blog_id):
+    """
+        Display Blog detail page
+
+        **Context**
+
+        ``blog``
+            An instance of :model:`Blog.blog`
+
+        **Templates**
+        :template:`pages/bloginside.html`
+    """
+    blogdetails = get_object_or_404(Blog, pk=blog_id)
+    blogs = Blog.objects.all()[:3]
+    context = {
+        'blogdetails': blogdetails,
+        'blogs': blogs
+    }
+    return render(request, 'pages/bloginside.html', context)
