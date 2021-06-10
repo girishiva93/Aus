916411781430a7f57bab54e7b683e026e8e1aa88
{shiva} <girishiva93@gmail.com>
2021-06-10 19:24:32 +0545

    Changing the footer description

--- AU/settings.py
+++ AU/settings.py
@@ -79,11 +79,13 @@ WSGI_APPLICATION = 'AU.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'ec2-34-195-233-155.compute-1.amazonaws.com',
+        'USER': 'cznmebxenoyfvl',
+        'PASSWORD': 'fe5ba6c8ca1402eaa827f5c0fda8d5c5ea057817a3d3229e6816e7f44efab293',
+        'NAME': 'd70t9kmepsustn',
     }
 }
 
